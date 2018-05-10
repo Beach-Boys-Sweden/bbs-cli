b8ae39d11254b7889ba626ab713d28fece391eb3
Gideon Oduro <gideon.oduro@tele2.com>
2018-05-10 14:36:42 +0200

    small fix

--- dist/config/resolve.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-exports.resolve = resolve;
-function resolve() {
-    return { extensions: [".js", ".css"] };
-}
